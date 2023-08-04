ff7e53ccac00ef059bb084b6b500f088a9a5b941
Andrej Dergavko <andrejdergavko@gmail.com>
2023-08-04 21:35:01 +0300

    chore: add route protection for unauthorized users

--- src/app/api/openai/chat-gpt/route.ts
+++ src/app/api/openai/chat-gpt/route.ts
@@ -1,9 +1,18 @@
 import { NextResponse } from 'next/server';
-import { chatgpt } from 'src/services/openai/apiReq';
+import { getServerSession } from 'next-auth';
+
+import { chatgpt } from '~services/openai/apiReq';
+import { authOptions } from '~api/auth/[...nextauth]/route';
 
 export const dynamic = 'force-dynamic';
 
 export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user.email || !session.user.imapAccessToken) {
+    throw new Error('User is not authenticated.');
+  }
+
   try {
     const { message } = await req.json();
     const assistantMessage = await chatgpt(message);
